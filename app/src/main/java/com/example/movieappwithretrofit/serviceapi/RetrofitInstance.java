693dc39a9320d6904cd41bc60d2cf8a1f7c0e301
Rahul <rahulbodh819@gmail.com>
2024-05-25 18:35:31 +0530

    create RetrofitInstance class

---
+++ app/src/main/java/com/example/movieappwithretrofit/serviceapi/RetrofitInstance.java
@@ -0,0 +1,24 @@
+package com.example.movieappwithretrofit.serviceapi;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    // Acts as a central configuration point for
+    // defining how HTTP requests and responses
+    // should be handled.
+
+    private static Retrofit retrofit = null;
+    private static String BASE_URL = "https://api.themoviedb.org/3/";    // base url
+
+    public static MovieApiService getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(MovieApiService.class);
+    }
+
+}
