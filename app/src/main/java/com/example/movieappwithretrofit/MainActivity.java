39355213c1dadfc155230457bac18b0c06bda44d
Rahul <rahulbodh819@gmail.com>
2024-05-26 21:58:37 +0530

    getting popular Movies from Api

--- app/src/main/java/com/example/movieappwithretrofit/MainActivity.java
+++ app/src/main/java/com/example/movieappwithretrofit/MainActivity.java
@@ -7,18 +7,78 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.example.movieappwithretrofit.databinding.ActivityMainBinding;
+import com.example.movieappwithretrofit.model.Movie;
+import com.example.movieappwithretrofit.view.MovieAdapter;
+import com.example.movieappwithretrofit.viewmodel.MainActivityViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Movie> movies;
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_main
+        );
+
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        getPopularMovies();
+
+        swipeRefreshLayout = binding.swipeLayout;
+        swipeRefreshLayout.setColorSchemeResources(R.color.black);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getPopularMovies();
+            }
+        });
+
+    }
+
+    private void getPopularMovies() {
+        viewModel.getAllMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> moviesFromLiveData) {
+                movies = (ArrayList<Movie>) moviesFromLiveData;
+                displayMoviesInRecyclerView();
+            }
         });
     }
+
+    private void displayMoviesInRecyclerView() {
+        recyclerView = binding.recyclerView;
+        movieAdapter = new MovieAdapter(this, movies);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+
+        // notify an adapter associated with a RecyclerView
+        // that the underlying dataset hase changed
+        movieAdapter.notifyDataSetChanged();
+
+
+    }
 }
+
+
